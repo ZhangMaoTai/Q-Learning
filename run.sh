e2765d2d32511ea54f13569b6f3da6487ee5e9e9
ZhangMaoTai <102578441+ZhangMaoTai@users.noreply.github.com>
2023-09-24 01:59:15 +0800

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 wandb login f96d4d9da75d32e2300789569921126efb16c9cc
 
 python main.py \
---max_size 128 \
---batch_size 64 \
+--max_size 64 \
+--batch_size 32 \
 --mini_epoch 1 \
 --num_updates 10000 \
 --learning_rate 3e-4 \
