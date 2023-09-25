f6a0fce1ea75379d2fba98dcd3bb5a71a1308747
zhangjiasheng <zhangjiasheng@xiaohongshu.com>
2023-09-26 02:15:10 +0800

    update

--- run_class.sh
+++ run_class.sh
@@ -1,8 +1,8 @@
 wandb login f96d4d9da75d32e2300789569921126efb16c9cc
 
 python main_class.py \
---train_batch_size 2048 \
---eval_batch_size 4096 \
+--train_batch_size 1024 \
+--eval_batch_size 2048 \
 --epochs 1 \
 --learning_rate 3e-4 \
 --num_warmup_steps 1000 \
